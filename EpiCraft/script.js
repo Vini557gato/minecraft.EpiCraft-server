b85c47542f25335fa34a83a4d41ed1548a71eba6
Vini557gato <vinithony378@gmail.com>
2025-09-21 17:19:47 -0400

    Delete EpiCraft directory

--- EpiCraft/script.js
+++
@@ -1,6 +0,0 @@
-// Exemplo: alerta quando clicar em um link
-document.querySelectorAll('nav a').forEach(link => {
-    link.addEventListener('click', () => {
-        alert("Você clicou em um link do EpiCraft!");
-    });
-});
