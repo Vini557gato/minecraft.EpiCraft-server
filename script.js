526aa38fb28d39089e54e2b99a8f6604c73ff818
Vini557gato <vinithony378@gmail.com>
2025-09-21 17:23:03 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,1 @@
-// Exemplo: alerta quando clicar em um link
-document.querySelectorAll('nav a').forEach(link => {
-    link.addEventListener('click', () => {
-        alert("Você clicou em um link do EpiCraft!");
-    });
-});
+
