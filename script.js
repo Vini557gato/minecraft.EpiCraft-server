f9a177fe15b5e9e9b8b2102331f16174b9637b88
Vini557gato <vinithony378@gmail.com>
2025-09-27 19:55:49 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,21 @@
+// Pegar todos os links e abas
+const links = document.querySelectorAll(".tablink");
+const abas = document.querySelectorAll(".aba");
 
+// Ativar aba ao clicar
+links.forEach(link => {
+  link.addEventListener("click", e => {
+    e.preventDefault();
+    const target = link.getAttribute("data-aba");
+
+    if (!target) return; // ignorar Discord
+
+    // Remover ativo de todas
+    links.forEach(l => l.classList.remove("active"));
+    abas.forEach(a => a.classList.remove("ativa"));
+
+    // Ativar clicada
+    link.classList.add("active");
+    document.getElementById(target).classList.add("ativa");
+  });
+});
